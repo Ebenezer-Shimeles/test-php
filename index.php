6473d14c994308acaf2c5246d20b3b824e4ac317
Ebenezer <40866268+Ebenezer-Shimeles@users.noreply.github.com>
2020-01-21 10:28:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
-      require_once 'Bot_lib\main.php';
-   $x = new bot("1032278958:AAGXNDBlidRywlwp0dCk5yLbhR8PdswdmZY");
-    //echo $x->getLastResponse();
-    //echo $x->sendMessage(556659349, "HELLO MAN!");
-    //echo $x->forwardMessage("556659349", "556659349", "31");
-    //header("Content-type: application/json");
-  //echo $x->sendMessage("-383337888", "when r we gonna practice geo n who's gonna present??");
- // 356941894 yerosun -383337888 geeo grp
-  $x->sendMessage("556659349", "new msg");
-?>
+

b842725bbb982e9e8b621b9d3855bb94eb41d698
Ebenezer <40866268+Ebenezer-Shimeles@users.noreply.github.com>
2020-01-21 13:57:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
    require getcwd()."/Bot_lib/main.php";
    $x = new bot("1032278958:AAGXNDBlidRywlwp0dCk5yLbhR8PdswdmZY");
-   echo $x->getUpdates();
+   echo $x->sendMessage("556659349", "New ALert");
 ?>

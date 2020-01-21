5c7b70b3173c916536c5fa451ab2a387643afbb6
Ebenezer <40866268+Ebenezer-Shimeles@users.noreply.github.com>
2020-01-21 14:56:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-   require getcwd()."/Bot_lib/main.php";
+   /*require getcwd()."/Bot_lib/main.php";
    $x = new bot("1032278958:AAGXNDBlidRywlwp0dCk5yLbhR8PdswdmZY");
-   echo $x->sendMessage("556659349", "New ALert");
+   echo $x->sendMessage("556659349", "New ALert");*/
+   $database = new SQLite3('myDatabase.sqlite');
 ?>

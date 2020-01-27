81c42395c56731b581c01167c33dfb96ededb479
Ebenezer <40866268+Ebenezer-Shimeles@users.noreply.github.com>
2020-01-27 20:13:19 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,7 @@
 <?php
-   echo var_dump($_GET);
+   require_once getcwd()."/Bot_lib/main.php";
+   $x  = new Bot("941266680:AAEXLAnOlcQ3IqIlRFL_598_uSRYwLU2yzg");
+   $x->sendMessage("556659349", var_dump($_GET));
 ?>
 
 

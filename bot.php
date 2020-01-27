7c677ef9e0c37f2f8104e9ad06edc4959b6616f5
Ebenezer <40866268+Ebenezer-Shimeles@users.noreply.github.com>
2020-01-27 20:27:04 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
    require_once getcwd()."/Bot_lib/main.php";
    $x  = new bot("941266680:AAEXLAnOlcQ3IqIlRFL_598_uSRYwLU2yzg");
-   echo $x->sendMessage("556659349", "GUTS OVE FEAR);
+   $x->sendMessage("556659349", "GUTS OVE FEAR);
 ?>
 
 

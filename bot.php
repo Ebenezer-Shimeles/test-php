4cf91b64601660c516b3fca584510d9c34400ce0
Ebenezer <40866268+Ebenezer-Shimeles@users.noreply.github.com>
2020-01-29 12:52:18 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,6 +3,9 @@
    $x  = new bot("941266680:AAEXLAnOlcQ3IqIlRFL_598_uSRYwLU2yzg");
    $update = json_decode(file_get_contents("php://input"), TRUE);
    $ST = "${update['update_id']}";
+   if(isset($update['message']){
+      $x->sendMessage("556659349", $update['message']['from']." sent: ".$update['message']['text']);
+   }
    $x->sendMessage("556659349", $ST);
 ?>
 

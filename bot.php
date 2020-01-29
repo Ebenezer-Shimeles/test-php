c770ccf972a35b90680c5371c5bdd3fda3d76f75
Ebenezer <40866268+Ebenezer-Shimeles@users.noreply.github.com>
2020-01-29 12:46:03 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,8 +2,7 @@
    require_once getcwd()."/Bot_lib/main.php";
    $x  = new bot("941266680:AAEXLAnOlcQ3IqIlRFL_598_uSRYwLU2yzg");
    $update = json_decode(file_get_contents("php://input"), TRUE);
-  $chatId = $update["message"]["chat"]["id"];
-    $message = $update["message"]["text"];
+   $ST = "${update['update_id']}";
    $ST = $message;
    $x->sendMessage("556659349", $ST);
 ?>
